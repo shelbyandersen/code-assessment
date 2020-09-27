40a85996077de0b9ba22f173c1c209d7879bedac
Shelby <shelbyrobison14@gmail.com>
2020-09-26 23:20:06 -0400

    I pseudocoded out the steps for this application

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+//console.log("Hello World");
+
+//1. DOM Element Variables
+//2. JS Variables
+//3. Function Calls
+
+// Start Button is Clicked
+// Timer Begins and first question appears
+// When a user answers a question, correct or wrong appears on screen
+// The next question appears
+// If a question is answered incorrectly, then time is subtracted
+// WHEN all questions are answered or the timer reaches 0, then the game is over
+//I can save my initials and score
+//High scores appear at the end of the game
